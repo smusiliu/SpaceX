6ca23027b312f918015401f2a9b54a92a08aa4e8
Samad Musiliu <musiliusamad@gmail.com>
2023-08-23 20:03:19 +0100

    added few changes to the rockets components

--- src/components/RocketCard.jsx
+++ src/components/RocketCard.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import { Card, CardBody, CardFooter, Image, Button, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure } from "@nextui-org/react";
+import { Card, CardBody, CardFooter, Image, Button, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure, Link } from "@nextui-org/react";
 
 const RocketCard = ({ rocket }) => {
     const { isOpen, onOpen, onClose } = useDisclosure();
@@ -35,7 +36,7 @@ const RocketCard = ({ rocket }) => {
                 </Button>
 
 
-                <Modal backdrop='blur' isOpen={isOpen} onClose={onClose}>
+                <Modal backdrop='blur' isOpen={isOpen} onClose={onClose} className='bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black text-gray-200'>
                     <ModalContent>
                         {(onClose) => (
                             <>
@@ -49,13 +50,48 @@ const RocketCard = ({ rocket }) => {
                                         className="w-full object-cover h-[200px]"
                                         src={rocket.flickr_images[2] ? rocket.flickr_images[0] : 'https://live.staticflickr.com/65535/53137348705_f34bcc2620_m.jpg'}
                                     />
-                                    <p className="text-sm">
+                                    <p className="text-[0.81rem] font-[300]">
                                         {rocket.description}
                                     </p>
+
+                                    <div className="grid grid-cols-3 gap-4 items-center place-items-center my-3 text-gray-400">
+                                        <div>
+                                            <p className=" text-sm"> Height </p>
+                                            <span className="text-xs"> {rocket.height.meters}m </span>
+                                        </div>
+                                        <div>
+                                            <p className=" text-sm"> Diameter </p>
+                                            <span className="text-xs"> {rocket.diameter.meters}m </span>
+                                        </div>
+                                        <div>
+                                            <p className=" text-sm"> Mass </p>
+                                            <span className="text-xs"> {rocket.mass.kg}kg </span>
+                                        </div>
+                                        <div>
+                                            <p className=" text-sm"> Type </p>
+                                            <span className="text-xs"> {rocket.type} </span>
+                                        </div>
+                                        <div>
+                                            <p className=" text-sm"> Active </p>
+                                            <span className="text-xs"> {rocket.height.active ? 'Yes' : 'No'} </span>
+                                        </div>
+                                        <div>
+                                            <p className=" text-sm"> Country </p>
+                                            <span className="text-xs"> {rocket.country} </span>
+                                        </div>
+
+                                    </div>
                                 </ModalBody>
                                 <ModalFooter>
-                                    <Button color="danger" variant="light" onPress={onClose}>
-                                        Close
+                                    <Button
+                                        as={Link}
+                                        color="primary"
+                                        href={rocket.wikipedia}
+                                        variant="solid"
+                                        onPress={() => handleOpen()}
+                                        className="capitalize text-[0.78rem] text-gray-300 w-full mt-3"
+                                    >
+                                        View More on Wikipedia
                                     </Button>
                                 </ModalFooter>
                             </>
--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -24,9 +24,8 @@ export default function Rockets() {
     return (
         <div className='max-w-[76rem] min-h-screen mx-auto my-20 text-gray-300 w-full'>
             <h2 className='text-center text-xl font-medium'>Search for Rockets</h2>
-            <p className='text-4xl'> {rockets.length} </p>
 
-            <div className='grid grid-cols-4 gap-4 w-full'>
+            <div className='grid grid-cols-4 gap-4 w-full mt-10'>
                 {rockets.map(rocket => (
                     <RocketCard
                         rocket={rocket}
