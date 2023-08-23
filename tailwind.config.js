95501eb187bcb79fdc58fa4376d3d6eaf45e5aac
Samad Musiliu <musiliusamad@gmail.com>
2023-08-23 07:45:26 +0100

    made a bit of formatting (indent)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 // const {nextui} = require("@nextui-org/react");
-import { nextui } from "@nextui-org/react/dist";
+import { nextui } from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
 export default {
